7e442d5169e9ce85d2d57e9462e658be4df5e081
proxar <pedroguerrero902@gmail.com>
2021-05-26 21:43:07 -0500

    Arreglo en validadores e inserts

--- validadores.ts
+++ validadores.ts
@@ -7,6 +7,7 @@ db.createCollection(
         bsonType: 'object',
         required: [
           'tipoDocumento',
+          'nombre',
           'documento',
           'ciudad',
           'genero',
@@ -19,6 +20,10 @@ db.createCollection(
             bsonType: 'string',
             enum: ["CC", "TI", "CE", "NIP", "NIT"],
             description: 'must be "CC" or "TI" or "CE" or "NIP" or "NIT" and is required'
+          },
+          nombre: {
+            bsonType: 'string',
+            description: 'must be a string and is required'
           },
           documento: {
             bsonType: 'string',
@@ -184,11 +189,11 @@ db.createCollection(
             description: 'must be a date and is required'
           },
           reaccion: {
-            bsonType: 'boolean',
+            bsonType: 'bool',
             description: 'must be a boolean and is required'
           },
           acompanhado: {
-            bsonType: 'boolean',
+            bsonType: 'bool',
             description: 'must be a boolean and is required'
           },
           vacuna: {
