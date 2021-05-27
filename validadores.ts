6190e78427fd3b93ec56892847d8e4af1f0cdb0b
dhasane <danihas@live.com>
2021-05-26 20:59:49 -0500

    Parece

--- validadores.ts
+++ validadores.ts
@@ -112,6 +112,108 @@ db.createCollection(
           asistira: {
             bsonType: 'boolean',
             description: 'must be a boolean and is required'
+          },
+          cita: {
+            bsonType: 'object',
+            required: [
+              'centroVacunacion',
+              'fecha'
+            ],
+            properties: {
+              centroVacunacion: {
+                bsonType: 'objectId',
+                description: 'must be a string and is required'
+              },
+              fecha: {
+                bsonType: 'date',
+                description: 'must be a date and is required'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+)
+
+db.centrosVacunacion.drop()
+db.createCollection(
+  'centrosVacunacion', {
+    validator: {
+      $jsonSchema: {
+        bsonType: 'object',
+        required: [
+          'nombre',
+          'ciudad'
+        ],
+        properties: {
+          nombre: {
+            bsonType: 'string',
+            description: 'must be a string and is required'
+          },
+          ciudad: {
+            bsonType: 'objectId',
+            description: 'must be an objectId and is required'
+          }
+        }
+      }
+    }
+  }
+)
+
+db.vacunaciones.drop()
+db.createCollection(
+  'vacunaciones', {
+    validator: {
+      $jsonSchema: {
+        bsonType: 'object',
+        required: [
+          'idLlamada',
+          'fecha',
+          'reaccion',
+          'acompanhado',
+          'vacuna'
+        ],
+        properties: {
+          idLlamada: {
+            bsonType: 'objectId',
+            description: 'must be a objectId and is required'
+          },
+          fecha: {
+            bsonType: 'date',
+            description: 'must be a date and is required'
+          },
+          reaccion: {
+            bsonType: 'boolean',
+            description: 'must be a boolean and is required'
+          },
+          acompanhado: {
+            bsonType: 'boolean',
+            description: 'must be a boolean and is required'
+          },
+          vacuna: {
+            bsonType: 'objectId',
+            description: 'must be a objectId and is required'
+          }
+        }
+      }
+    }
+  }
+)
+
+db.eps.drop()
+db.createCollection(
+  'eps', {
+    validator: {
+      $jsonSchema: {
+        bsonType: 'object',
+        required: [
+          'nombre'
+        ],
+        properties: {
+          nombre: {
+            bsonType: 'string',
+            description: 'must be a string and is required'
           }
         }
       }
@@ -139,9 +241,9 @@ db.createCollection(
   }
 )
 
-db.vacuna.drop()
+db.vacunas.drop()
 db.createCollection(
-  'vacuna', {
+  'vacunas', {
     validator: {
       $jsonSchema: {
         bsonType: 'object',
@@ -159,62 +261,33 @@ db.createCollection(
   }
 )
 
-db.vacuna.drop()
-db.createCollection(
-  'vacuna', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        required: [
-          'nombre'
-        ],
-        properties: {
-          nombre: {
-            bsonType: 'string',
-            description: 'must be a string and is required'
-          }
-        }
-      }
-    }
+db.vacunas.insert(
+  {
+    nombre: "astraZeneca"
   }
 )
 
-db.cita.drop()
-db.createCollection(
-  'cita', {
-    validator: {
-      $jsonSchema: {
-        bsonType: 'object',
-        required: [
-          'identificador',
-          'centroVacunacion',
-          'persona',
-          'fecha'
-        ],
-        properties: {
-          nombre: {
-            bsonType: 'string',
-            description: 'must be a string and is required'
-          }
-          identificador: {
-            bsonType: 'string',
-            description: 'must be a string and is required'
-          },
-          centroVacunacion: {
-            bsonType: 'objectId',
-            description: 'must be a string and is required'
-          },
-          persona: {
-            bsonType: 'objectId',
-            description: 'must be a string and is required'
-          },
-          fecha: {
-            bsonType: 'date',
-            description: 'must be a date and is required'
-          }
-        }
-      }
-    }
+db.ciudad.insert(
+  {
+    nombre: "bogota"
+  }
+)
+
+db.eps.insert(
+  {
+    nombre: "sanitas"
+  }
+)
+
+db.centrosVacunacion.insert(
+  {
+    nombre: "Movistar arena",
+    ciudad: db.ciudad.findOne(
+      {
+        nombre: "bogota",
+      },
+      {_id: 1}
+    )._id
   }
 )
 
@@ -222,11 +295,21 @@ db.personas.insert(
   {
     tipoDocumento: "CC",
     documento: "123412123",
-    ciudad: ObjectId("609ff0eabda506ca3c35e07c"),
+    ciudad: db.ciudad.findOne(
+      {
+        nombre: "bogota",
+      },
+      {_id: 1}
+    )._id,
     genero: "genero",
     edad: 23,
     eps: {
-      eps: ObjectId("609ff0eabda506ca3c35e07c"),
+      eps: db.ciudad.findOne(
+      {
+        nombre: "bogota",
+      },
+      {_id: 1}
+    )._id,
       fechaAfiliacion: new Date(),
       tipo: "cotizante"
     },
@@ -239,37 +322,52 @@ db.personas.insert(
   }
 )
 
+var fecha = new Date()
+
+var usr = db.personas.findOne(
+  {
+    tipoDocumento: "CC",
+    documento:  "123412123"
+  },
+  {_id: 1}
+)._id
+
 db.llamadas.insert(
   {
-    date: new Date(),
-    persona: db.personas.findOne(
-      {
-        tipoDocumento: "CC",
-        documento:  "123412123"
-      },
-      {_id: 1}
-    )._id,
+    date: fecha,
+    persona: usr,
     numero: 1,
     acepto: false,
-    asistira: true
+    asistira: true,
+    cita: {
+      centroVacunacion: db.centrosVacunacion.findOne(
+        {
+          nombre: "Movistar arena"
+        },
+        {_id: 1}
+      )._id,
+      fecha: new Date()
+    }
   }
 )
 
-cita
-{
-  identificador: ,
-  centroVacunacion:
-  persona: objectId
-  fecha: date
-}
-
-
-
-vacunacion
-{
-  idCita: objectId,
-  día y hora de la vacuna,,
-  si tuvo reacción en los siguientes quince minutos,
-  si fue acompañado o no y por quien.,
-  vacuna: objectId
-}
+db.vacunaciones.insert(
+  {
+    idLlamada: db.llamadas.find(
+      {
+        date: fecha,
+        persona: usr
+      }
+    ),
+    fecha: new Date(),
+    reaccion: false,
+    acompanhado: true,
+    vacuna:
+      db.vacunas.findOne(
+        {
+          nombre: "astraZeneca"
+        },
+        {_id: 1}
+      )._id
+  }
+)
