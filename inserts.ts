50ecef18fdec3964f40e66903e69357db1938752
dhasane <danihas@live.com>
2021-05-26 23:10:47 -0500

    Agregar vacuna a segunda persona

--- inserts.ts
+++ inserts.ts
@@ -61,6 +61,38 @@ db.personas.insert(
   }
 )
 
+db.personas.insert(
+  {
+    tipoDocumento: "CC",
+    nombre: "cualquier nombre",
+    documento: "1234123452343",
+    ciudad: db.ciudad.findOne(
+      {
+        nombre: "bogota",
+      },
+      {_id: 1}
+    )._id,
+    genero: "mujer",
+    edad: 23,
+    eps: {
+      eps: db.ciudad.findOne(
+      {
+        nombre: "bogota",
+      },
+      {_id: 1}
+    )._id,
+      fechaAfiliacion: new Date(),
+      tipo: "cotizante"
+    },
+    telefonos: [
+      {
+        descripcion: "descripcion",
+        numero: "numesdfasdfarooo"
+      }
+    ]
+  }
+)
+
 var fecha = new Date()
 
 var usr = db.personas.findOne(
@@ -70,6 +102,33 @@ var usr = db.personas.findOne(
   },
   {_id: 1}
 )._id
+
+var usr2 = db.personas.findOne(
+  {
+    tipoDocumento: "CC",
+    documento:  "1234123452343"
+  },
+  {_id: 1}
+)._id
+
+db.llamadas.insert(
+  {
+    date: fecha,
+    persona: usr2,
+    numero: 1,
+    acepto: false,
+    asistira: true,
+    cita: {
+      centroVacunacion: db.centrosVacunacion.findOne(
+        {
+          nombre: "Movistar arena"
+        },
+        {_id: 1}
+      )._id,
+      fecha: new Date()
+    }
+  }
+)
 
 db.llamadas.insert(
   {
@@ -87,6 +146,28 @@ db.llamadas.insert(
       )._id,
       fecha: new Date()
     }
+  }
+)
+
+db.vacunaciones.insert(
+  {
+    idLlamada: db.llamadas.findOne(
+      {
+        date: fecha,
+        persona: usr2
+      },
+      {_id: 1}
+    )._id,
+    fecha: new Date(),
+    reaccion: false,
+    acompanhado: true,
+    vacuna:
+      db.vacunas.findOne(
+        {
+          nombre: "astraZeneca"
+        },
+        {_id: 1}
+      )._id
   }
 )
 
