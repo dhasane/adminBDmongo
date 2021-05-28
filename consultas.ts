62f552b15962d93b5c4e2d013b14f07db50196c8
dhasane <danihas@live.com>
2021-05-28 17:58:11 -0500

    Poner reacciones en el segundo punto

--- consultas.ts
+++ consultas.ts
@@ -48,36 +48,37 @@ db.vacunaciones.aggregate(
   ]).pretty()
 
 // punto 2
-db.vacunaciones.find().forEach( (elem) =>
+db.vacunaciones.find().forEach( (elem) => {
+  var idVacuna = elem.vacuna;
+  var nombre_vacuna = db.vacunas.findOne({_id: idVacuna},{_id:0,nombre:1});
+
+  var id_llamada = elem.idLlamada;
+  var id_persona = db.llamadas.findOne({_id: id_llamada}, {_id:0,persona:1, "cita.centroVacunacion":1});
+  var id_centroVacunacion = id_persona.cita.centroVacunacion;
+  var centroVacunacion = db.centrosVacunacion.findOne({_id: id_centroVacunacion},{_id:0,nombre:1});
+
+  var datos_persona = db.personas.findOne({_id: id_persona.persona},{_id:0,nombre: 1, documento: 1, edad:1});
+
+  var fecha = elem.fecha;
+
+  var id_ciudad = db.personas.findOne({_id: id_persona.persona},{_id:0, ciudad:1});;
+  var ciudad = db.ciudad.findOne({_id: id_ciudad.ciudad},{_id:0, nombre:1});
+
+  var reacciones_vacuna = db.vacunaciones.findOne({idLlamada: id_llamada}).reacciones;
+
+  printjson(
     {
-        var idVacuna = elem.vacuna;
-        var nombre_vacuna = db.vacunas.findOne({_id: idVacuna},{_id:0,nombre:1});
-
-        var id_llamada = elem.idLlamada;
-        var id_persona = db.llamadas.findOne({_id: id_llamada}, {_id:0,persona:1, "cita.centroVacunacion":1});
-        var id_centroVacunacion = id_persona.cita.centroVacunacion;
-        var centroVacunacion = db.centrosVacunacion.findOne({_id: id_centroVacunacion},{_id:0,nombre:1});
-
-        var datos_persona = db.personas.findOne({_id: id_persona.persona},{_id:0,nombre: 1, documento: 1, edad:1});
-
-        var fecha = elem.fecha;
-
-        var id_ciudad = db.personas.findOne({_id: id_persona.persona},{_id:0, ciudad:1});;
-        var ciudad = db.ciudad.findOne({_id: id_ciudad.ciudad},{_id:0, nombre:1}); 
-
-        printjson(
-            {
-                vacuna: nombre_vacuna.nombre,
-                nombre: datos_persona.nombre,
-                documento: datos_persona.documento,
-                edad: datos_persona.edad,
-                fecha_de_vacunacion: fecha,
-                ciudad: ciudad.nombre,
-                centro_vacunacion: centroVacunacion.nombre
-            }
-        )
+      vacuna: nombre_vacuna.nombre,
+      nombre: datos_persona.nombre,
+      documento: datos_persona.documento,
+      edad: datos_persona.edad,
+      fecha_de_vacunacion: fecha,
+      ciudad: ciudad.nombre,
+      centro_vacunacion: centroVacunacion.nombre,
+      reacciones: reacciones_vacuna
     }
-)
+  )
+})
 
 // punto 4
 db.vacunaciones.aggregate(
