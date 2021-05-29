edc77bd25c03dd15fce7aed8b3066d5a27d6f214
proxar <pedroguerrero902@gmail.com>
2021-05-28 20:33:02 -0500

    segundo commir de insert personas

--- inserts.ts
+++ inserts.ts
@@ -600,3 +600,821 @@ db.vacunaciones.insertOne({idLlamada:"", fecha: new Date("2021, 03, 04"), reacci
   db.vacunaciones.insertOne({idLlamada:"", fecha: new Date("2021, 08, 24"), reacciones: ["Dolor de cuerpo"], acompanhado: false, vacuna: db.vacunas.findOne({nombre:"Tétanos"})})
   db.vacunaciones.insertOne({idLlamada:"", fecha: new Date("2021, 10, 03"), reacciones: ["Dolor de rodillas"], acompanhado: true, vacuna: db.vacunas.findOne({nombre:"Rotavirus"})})
   
+  db.personas.insertOne(
+    {
+      tipoDocumento: "CC",
+      nombre: "Albin Yakitory",
+      documento: "123412123",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Bogota",
+        },
+        {_id: 1}
+      )._id,
+      genero: "hombre",
+      edad: 23,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Sanitas",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "cotizante"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3168670976"
+        },
+         {
+          descripcion: "telefono fijo",
+          numero: "7337890"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3134145670"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "CC",
+      nombre: "Ariel",
+      documento: "100032290",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Bogota",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 18,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Cafesalud",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3153122403"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7583348"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3123207834"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "TI",
+      nombre: "Martin",
+      documento: "1191161400",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Medellin",
+        },
+        {_id: 1}
+      )._id,
+      genero: "hombre",
+      edad: 10,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Cafesalud",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "300224830924"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7485358"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3002891423"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "NIT",
+      nombre: "Camilo",
+      documento: "41911614",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Cali",
+        },
+        {_id: 1}
+      )._id,
+      genero: "hombre",
+      edad: 27,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Unimeq",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "cotizante"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3163122315"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7283456"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3145768906"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "CC",
+      nombre: "Jazmin",
+      documento: "7223401",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Bogota",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 32,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Compensar",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3167098645"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7324908"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3173178868"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "CE",
+      nombre: "Tiana",
+      documento: " 809746593",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Duitama",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 45,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Coomeva",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "cotizante"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3124142683"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7317645"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3045679052"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "TI",
+      nombre: "Omar",
+      documento: "104383528",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Duitama",
+        },
+        {_id: 1}
+      )._id,
+      genero: "hombre",
+      edad: 10,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Coomeva",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "cotizante"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3124142683"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7317645"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3045679052"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "NIP",
+      nombre: "Daniel",
+      documento: "635271894",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Armenia",
+        },
+        {_id: 1}
+      )._id,
+      genero: "hombre",
+      edad: 100,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Famisanar",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3218453632"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7245678"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3143121523"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "TI",
+      nombre: "Diana",
+      documento: "1001823479",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Bucaramanga",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 10,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Coomeva",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "cotizante"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3124142683"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7317645"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3045679052"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "NIP",
+      nombre: "Alba",
+      documento: "6723603",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Cali",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 33,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Cafesalud",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3154146730"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7583345"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3016756617"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "NIT",
+      nombre: "Esteban",
+      documento: "1127342619",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Medellin",
+        },
+        {_id: 1}
+      )._id,
+      genero: "hombre",
+      edad: 23,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Sanitas",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "cotizante"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3124142683"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7317645"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3045679052"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "CC",
+      nombre: "Elizabeth",
+      documento: "1001823479",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Duitama",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 18,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Unimeq",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3213587690"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7314673"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3056784213"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "NIP",
+      nombre: "Jacobo",
+      documento: "1145237894",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Medellin",
+        },
+        {_id: 1}
+      )._id,
+      genero: "hombre",
+      edad: 10,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Coomeva",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3167574358"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7402122"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3185462222"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "NIP",
+      nombre: "Luna",
+      documento: "1010238829",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Armenia",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 21,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Cafesalud",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3113008695"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7555555"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3220045210"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "TI",
+      nombre: "Mariana",
+      documento: "1423861302",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Cali",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 10,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Unimeq",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3162285205"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7508221"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3163989874"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "CC",
+      nombre: "Javier",
+      documento: "44311819",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Bogota",
+        },
+        {_id: 1}
+      )._id,
+      genero: "hombre",
+      edad: 70,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Famisanar",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "cotizante"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3182431828"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7799377"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3137799693"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "CE",
+      nombre: "Amelia",
+      documento: "8608381584",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Medellin",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 25,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Famisanar",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3022653008"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7530065"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3132137350"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "TI",
+      nombre: "Alejandra",
+      documento: "1000652926",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Duitama",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 16,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Sanitas",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "beneficiario"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3122580338"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7319046"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3187526244"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "CC",
+      nombre: "Javier",
+      documento: "44311819",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Bogota",
+        },
+        {_id: 1}
+      )._id,
+      genero: "hombre",
+      edad: 70,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Famisanar",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "cotizante"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3182431828"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7799377"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3137799693"
+        }
+      ]
+    }
+  )
+  
+  
+  db.personas.insertOne(
+    {
+      tipoDocumento: "CE",
+      nombre: "Laura",
+      documento: "1020664210",
+      ciudad: db.ciudad.findOne(
+        {
+          nombre: "Armenia",
+        },
+        {_id: 1}
+      )._id,
+      genero: "mujer",
+      edad: 22,
+      eps: {
+        eps: db.eps.findOne(
+        {
+          nombre: "Compensar",
+        },
+        {_id: 1}
+      )._id,
+        fechaAfiliacion: new Date(),
+        tipo: "cotizante"
+      },
+      telefonos: [
+        {
+          descripcion: "numero celular",
+          numero: "3113466970"
+        },
+        {
+          descripcion: "telefono fijo",
+          numero: "7326678"
+        },
+        {
+          descripcion: "numero contacto",
+          numero: "3125754155"
+        }
+      ]
+    }
+  )
